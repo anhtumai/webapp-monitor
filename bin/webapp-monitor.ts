728e82d5ebcffc1a040cc702f2e169c8201ad6c0
anhtumai <tu.mai@outlook.com>
2023-05-05 22:23:25 +0300

    init cdk project

--- bin/webapp-monitor.ts
+++ bin/webapp-monitor.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { WebappMonitorStack } from '../lib/webapp-monitor-stack';
+import * as cdk from "aws-cdk-lib";
+import { WebappMonitorStack } from "../lib/webapp-monitor-stack";
 
 const app = new cdk.App();
-new WebappMonitorStack(app, 'WebappMonitorStack');
+new WebappMonitorStack(app, "WebappMonitorStack", {
+  env: {
+    region: "eu-central-1",
+  },
+});
--- lib/webapp-monitor-stack.ts
+++ lib/webapp-monitor-stack.ts
@@ -1,19 +1,36 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import path = require("path");
+
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import {
+  LogLevel,
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
 
 export class WebappMonitorStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'WebappMonitorQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const webMonitorLambda = new NodejsFunction(this, "web-monitor-lambda", {
+      functionName: "web-monitor-lambda",
+
+      description: "Lambda for monitoring",
+      entry: path.join(__dirname, "../lambdas/webMonitoringHandler.ts"),
+      handler: "handler",
+
+      timeout: Duration.seconds(300),
+      runtime: Runtime.NODEJS_18_X,
+      environment: {},
+      bundling: {
+        externalModules: ["aws-sdk"],
+        target: "es2021",
+        logLevel: LogLevel.ERROR,
+        minify: true,
+        keepNames: true,
+        sourceMap: false,
+      },
     });
-
-    const topic = new sns.Topic(this, 'WebappMonitorTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }

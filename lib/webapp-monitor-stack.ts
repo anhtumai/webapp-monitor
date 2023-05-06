d87c3bceefe256e1d898f7757f5f37495e2c230c
anhtumai <tu.mai@outlook.com>
2023-05-06 18:41:16 +0300

    add form validator for app config

--- lib/webapp-monitor-stack.ts
+++ lib/webapp-monitor-stack.ts
@@ -8,10 +8,101 @@ import {
   NodejsFunctionProps,
 } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import * as appconfig from "aws-cdk-lib/aws-appconfig";
+
+class WebMonitorAppConfig extends Construct {
+  constructor(scope: Construct) {
+    const id = "web-monitor-app-config";
+    super(scope, id);
+
+    const webMonitorAppConfigApp: appconfig.CfnApplication =
+      new appconfig.CfnApplication(this, "web-monitor-app-config-app", {
+        name: "web-monitor-app-config-app",
+      });
+
+    const immediateDeploymentStrategy = new appconfig.CfnDeploymentStrategy(
+      this,
+      "web-monitor-deployment-strategy",
+      {
+        name: "web-monitor-deployment-strategy",
+        deploymentDurationInMinutes: 0,
+        growthFactor: 100,
+        replicateTo: "NONE",
+        finalBakeTimeInMinutes: 0,
+      },
+    );
+
+    const webMonitorAppConfigEnv: appconfig.CfnEnvironment =
+      new appconfig.CfnEnvironment(this, "web-monitor-app-config-env", {
+        applicationId: webMonitorAppConfigApp.ref,
+        name: "Production",
+      });
+
+    const webMonitorAppConfigProfile: appconfig.CfnConfigurationProfile =
+      new appconfig.CfnConfigurationProfile(
+        this,
+        "web-monitor-configuration-profile",
+        {
+          name: "web-monitor-configuration-profile",
+          applicationId: webMonitorAppConfigApp.ref,
+          locationUri: "hosted",
+          type: "AWS.Freeform",
+          validators: [
+            {
+              content: JSON.stringify({
+                type: "array",
+                items: {
+                  type: "object",
+                  required: ["url", "request", "rules"],
+                  additionalProperties: false,
+                  properties: {
+                    url: { type: "string" },
+                    request: { type: "string" },
+                    rules: {
+                      type: "array",
+                      items: {
+                        type: "object",
+                        properties: {
+                          containText: { type: "string" },
+                        },
+                      },
+                    },
+                  },
+                },
+              }),
+              type: "JSON_SCHEMA",
+            },
+          ],
+        },
+      );
+
+    const webMonitorConfigVersion: appconfig.CfnHostedConfigurationVersion =
+      new appconfig.CfnHostedConfigurationVersion(
+        this,
+        "web-monitor-hosted-configuration-version",
+        {
+          applicationId: webMonitorAppConfigApp.ref,
+          configurationProfileId: webMonitorAppConfigProfile.ref,
+          contentType: "application/json",
+          content: JSON.stringify("[]"),
+        },
+      );
+
+    new appconfig.CfnDeployment(this, "web-monitor-app-config-deployment", {
+      applicationId: webMonitorAppConfigApp.ref,
+      configurationProfileId: webMonitorAppConfigProfile.ref,
+      configurationVersion: webMonitorConfigVersion.ref,
+      deploymentStrategyId: immediateDeploymentStrategy.ref,
+      environmentId: webMonitorAppConfigEnv.ref,
+    });
+  }
+}
 
 export class WebappMonitorStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const webMonitorAppConfig = new WebMonitorAppConfig(this);
 
     const webMonitorLambda = new NodejsFunction(this, "web-monitor-lambda", {
       functionName: "web-monitor-lambda",

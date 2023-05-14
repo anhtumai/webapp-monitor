b7204da2750e9a74f64b0eddfb069dae19a787a3
anhtumai <tu.mai@outlook.com>
2023-05-14 21:03:52 +0300

    rename log with report in lambdas

--- lambdas/getUrlReportContents/config.ts
+++ lambdas/getUrlReportContents/config.ts
@@ -1,5 +1,5 @@
-export const LOG_SAVING_TABLE_NAME =
-  process.env.LOG_SAVING_TABLE_NAME || "web-monitor-table";
+export const REPORT_SAVING_TABLE_NAME =
+  process.env.REPORT_SAVING_TABLE_NAME || "web-monitor-table";
 
-export const LOG_SAVING_REGIONS =
-  process.env.LOG_SAVING_REGIONS?.split(",") || [];
+export const REPORT_SAVING_REGIONS =
+  process.env.REPORT_SAVING_REGIONS?.split(",") || [];
